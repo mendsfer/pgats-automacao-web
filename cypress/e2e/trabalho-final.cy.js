f77225ab8b26b551c81aebe506de2507d1402342
mendsfer <nandah.msi@gmail.com>
2025-11-02 19:14:34 -0300

    adicionando trabalho final

--- cypress/e2e/trabalho-final.cy.js
+++ cypress/e2e/trabalho-final.cy.js
@@ -3,6 +3,7 @@
 const dayjs = require("dayjs");
 
 import userData from '../fixtures/example.json';
+import creditCard from '../fixtures/order/creditCard.json';
 import { faker } from '@faker-js/faker'
 import login from '../modules/login/index'
 
@@ -11,12 +12,6 @@ describe('Automation Exercise', () => {
         cy.visit('https://www.automationexercise.com');
         cy.get('a[href="/login"]').click();
     });
-
-    it('Exemplos de Logs', () => {
-        cy.log(`STEP 1 :: PGATS AUTOMACÃO WEB CY LOG`)
-
-        cy.log(`Nome do usuário: ${userData.name}`)
-    })
 
     it('Test Case 1: Register User', () => {
         const timestamp = new Date().getTime();
@@ -35,9 +30,9 @@ describe('Automation Exercise', () => {
         cy.get('input[type="checkbox"]#newsletter').check();
         cy.get('input#optin').click();
 
-        cy.get('input#first_name').type(faker.person.firstName);
-        cy.get('input#last_name').type(faker.person.lastName);
-        cy.get('input#company').type(`PGATS ${faker.company.name}`);
+        cy.get('input#first_name').type(faker.person.firstName());
+        cy.get('input#last_name').type(faker.person.lastName());
+        cy.get('input#company').type(`PGATS ${faker.company.name()}`, { parseSpecialCharSequences: false });
         cy.get('input#address1').type('Avenida Selenium, n 2004');
         cy.get('select#country').select('Canada');
         cy.get('input#state').type('California');
@@ -57,7 +52,6 @@ describe('Automation Exercise', () => {
         cy.url().should('includes', 'delete_account');
         cy.contains('b', 'Account Deleted!')
         cy.get('[data-qa="continue-button"]').click();
-
     });
 
     it('Test Case 2: Login User with correct email and password', () => {
@@ -110,7 +104,7 @@ describe('Automation Exercise', () => {
 
 })
 
-describe ('Continuação dos exercícios', () => {
+describe('Continuação dos exercícios', () => {
 
     it('Test Case 6: Contact Us Form', () => {
 
@@ -121,8 +115,6 @@ describe ('Continuação dos exercícios', () => {
         cy.get('[data-qa="subject"]').type('Test Case 6: Contact Us Form');
         cy.get('[data-qa="message"]').type('Exercicio Test Case 6: Contact Us Form ');
 
-        // cy.get('[name="upload_file"]').selectFile('cypress/fixtures/logo2.png')
-
         cy.fixture('logo.png').as('arquivo')
         cy.get('[name="upload_file"]').selectFile('@arquivo')
 
@@ -131,17 +123,113 @@ describe ('Continuação dos exercícios', () => {
         cy.get('span').click();
     });
 
-    it('Test Case 7: Verify Test Cases Page', () => {
-
-        cy.visit('https://www.automationexercise.com');
-        cy.get('a[href="/test_cases"]').first().should('be.visible').click();
-        cy.get('a[href="/test_cases"]').should('contain', 'Test Cases');
-
-    });
 
     it('Test Case 8: Verify All Products and product detail page', () => {
 
-         cy.visit('https://www.automationexercise.com');
-        
+        cy.visit('https://www.automationexercise.com');
+        cy.get('a[href="/products"]').should('be.visible').click();
+
+        // cy.get('#meu-elemento')
+        //     .scrollIntoView()
+        //     .should('be.visible');
+        // cy.get('selector').scrollTo('center')
+        cy.get('.title').should('contain', 'All Products')
+        cy.get('a[href="/product_details/1"]').should('be.visible').click();
+        cy.get('.product-information').should('contain', 'Blue Top');
     })
+
+    it('Test Case 9: Search Product', () => {
+
+        cy.visit('https://www.automationexercise.com');
+        cy.get('a[href="/products"]').should('be.visible').click();
+
+
+        cy.get('.title').should('contain', 'All Products');
+        cy.get('#search_product').type('Blue Top');
+        cy.get('#submit_search').click();
+        cy.get('.title').should('contain', 'Searched Products');
+        cy.get('.productinfo > p').should('contain', 'Blue Top');
+    })
+
+    it('Test Case 10: Verify Subscription in home page', () => {
+
+        cy.visit('https://www.automationexercise.com');
+
+        cy.get('.single-widget > h2')
+            .scrollIntoView()
+            .should('be.visible');
+        cy.get('#susbscribe_email').type(userData.email);
+        cy.get('#subscribe').click();
+        cy.get('#success-subscribe').should('contain', 'You have been successfully subscribed!');
+    })
+
+    it('Test Case 15: Place Order: Register before Checkout', () => {
+
+        cy.visit('https://www.automationexercise.com');
+        cy.get('a[href="/login"]').click();
+
+        const timestamp = new Date().getTime();
+        const todayDate = dayjs(Date.now());
+
+        cy.get('[data-qa="signup-name"]').type('QA Tester');
+        cy.get('[data-qa="signup-email"]').type(`qa-tester-${timestamp}@test.com`);
+        cy.contains('button', 'Signup').click();
+
+        cy.get('input[type="radio"]').check('Mrs');
+        cy.get('input#password').type('12345', { log: false });
+        cy.get('#days').select(todayDate.daysInMonth());
+        cy.get('#months').select(todayDate.month());
+        cy.get('[data-qa="years"]').select('1992');
+
+        cy.get('input[type="checkbox"]#newsletter').check();
+        cy.get('input#optin').click();
+
+        cy.get('input#first_name').type(faker.person.firstName());
+        cy.get('input#last_name').type(faker.person.lastName());
+        cy.get('input#company').type(`PGATS ${faker.company.name()}`, { parseSpecialCharSequences: false });
+        cy.get('input#address1').type('Avenida Selenium, n 2004');
+        cy.get('select#country').select('Canada');
+        cy.get('input#state').type('California');
+        cy.get('input#city').type('Los Angeles');
+        cy.get('[data-qa="zipcode"]').type('90001');
+        cy.get('[data-qa="mobile_number"]').type('111 222 333');
+
+        cy.get('[data-qa="create-account"]').click();
+
+        cy.url().should('includes', 'account_created');
+        cy.contains('b', 'Account Created!')
+
+        cy.get('[data-qa="continue-button"]').click();
+
+        cy.contains('b', 'QA Tester');
+        cy.contains(`Logged in as QA Tester`).should('be.visible');
+
+        cy.get('[href="/product_details/3"]')
+            .scrollIntoView()
+            .click()
+
+        cy.contains('button', 'Add to cart').click()
+        cy.contains('a', 'View Cart').click()
+        cy.contains('a', 'Proceed To Checkout').click()
+        cy.contains('h2', 'Review Your Order').should('be.visible')
+        cy.contains('a', 'Place Order').click()
+
+        cy.fixture('order/creditCard').then((creditCard) => {
+            cy.get('input[data-qa=name-on-card]').type(creditCard.nameOnCard)
+            cy.get('input[data-qa=card-number]').type(creditCard.cardNumber)
+            cy.get('input[data-qa=cvc]').type(creditCard.cvc)
+            cy.get('input[data-qa=expiry-month]').type(creditCard.expirationMonth)
+            cy.get('input[data-qa=expiry-year]').type(creditCard.expirationYear)
+        })
+
+        cy.contains('button', 'Pay and Confirm Order').click()
+        cy.contains('Congratulations! Your order has been confirmed!').should('be.visible')
+        cy.get('a[href="/delete_account"]').click();
+
+        cy.url().should('includes', 'delete_account');
+        cy.contains('b', 'Account Deleted!')
+        cy.get('[data-qa="continue-button"]').click();
+    
+    })
+
 })
